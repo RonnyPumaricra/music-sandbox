d15c5163ad2cb87b9986f81406f2552e8b94ddf1
RonnyPumaricra <ronnylpqwerty@gmail.com>
2024-02-16 17:24:23 -0500

    Innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+
+function App() {
+  return <>
+    <div>Title component</div>
+    <p>Music Sandbox</p>
+
+    <div className="chords-only">
+      chords collection
+    </div>
+    
+    <div>Piano component</div>
+    <div>Guitar component</div>
+  </>
+}
+
+export default App
